3ec7ed3cbd66f652ec05e3bd1e1cea88df983faa
skylordants <antsupoiss@gmail.com>
2017-02-10 17:25:30 +0200

    Working little charactercontroller with a small scene

--- Assets/Scripts/Character/CharController.cs
+++ Assets/Scripts/Character/CharController.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class CharController : MonoBehaviour
 {
+	CharacterController controller;
+
+	public float walkspeed = 0.01f;
+
 
 	void Start ()
 	{
-		
+		controller = GetComponent<CharacterController> ();
 	}
 	
 	void Update ()
 	{
-		
+		Vector3 direction = new Vector3 (Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		Vector3 movement = Vector3.Normalize(direction) * walkspeed;
+
+		controller.Move (movement);
+
 	}
 }
