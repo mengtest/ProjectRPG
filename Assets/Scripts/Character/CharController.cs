7e633f51105022f2229a043d4cab03d956ca1e4d
skylordants <antsupoiss@gmail.com>
2017-02-13 19:40:41 +0200

    Created stamina and health system, updated world, created UI

--- Assets/Scripts/Character/CharController.cs
+++ Assets/Scripts/Character/CharController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum PlayerMoveType { Walk, Run, Sneak };
 
@@ -9,15 +10,79 @@ public class CharController : MonoBehaviour
 	CharacterController controller;
 	PlayerMoveType playerMoveType = PlayerMoveType.Walk;
 
+	private float health;
+	public float startingHealth = 100;
+	public float maxHealth;
+	public GameObject healthCounter;
+	public GameObject healthCounterBackground;
+
+	public float Health
+	{
+		get
+		{
+			return health;
+		}
+
+		set
+		{
+			health = Mathf.Clamp (value, 0, maxHealth);
+			UpdateHealth ();
+		}
+	}
+
+
+	public float stamina;
+	public float startingStamina = 100;
+	public float maxStamina;
+	public float staminaGenerationPerSecond = 1;
+	public GameObject staminaCounter;
+	public GameObject staminaCounterBackground;
+
+	public float Stamina
+	{
+		get
+		{
+			return stamina;
+		}
+
+		set
+		{
+			stamina = Mathf.Clamp( value, 0, maxStamina);
+			UpdateStamina ();
+		}
+	}
+
+
 	public float walkspeed = 0.07f;
 	public float runspeed = 0.12f;
+	public float staminaLostForRunning = 0.2f;
 	public float sneakSpeed = 0.03f;
+
+	//public float timeToAttack = 0.1f;
+	//public float attackTime = 0.5f;
+	//public float timeAfterAttack = 0.1f;
+	//public float weaponDamage = 1;
+
 
 	float moveSpeed;
 
 
 	void Start ()
 	{
+		health = startingHealth;
+
+		stamina = startingStamina;
+
+		if (maxHealth == 0)
+		{
+			maxHealth = startingHealth;
+		}
+
+		if (maxStamina == 0)
+		{
+			maxStamina = startingStamina;
+		}
+
 		moveSpeed = walkspeed;
 
 		controller = GetComponent<CharacterController> ();
@@ -32,7 +97,7 @@ public class CharController : MonoBehaviour
 				playerMoveType = PlayerMoveType.Sneak;
 				moveSpeed = sneakSpeed;
 			}
-			else if (Input.GetButton ("Run"))
+			else if (Input.GetButton ("Run") && Stamina > 0)
 			{
 				playerMoveType = PlayerMoveType.Run;
 				moveSpeed = runspeed;
@@ -45,7 +110,7 @@ public class CharController : MonoBehaviour
 		}
 		else if (playerMoveType != PlayerMoveType.Sneak)
 		{
-			if (Input.GetButton ("Run"))
+			if (Input.GetButton ("Run") && Stamina > 0.1f)
 			{
 				playerMoveType = PlayerMoveType.Run;
 				moveSpeed = runspeed;
@@ -63,5 +128,25 @@ public class CharController : MonoBehaviour
 
 		controller.Move (movement);
 
+		if (playerMoveType == PlayerMoveType.Run && movement != Vector3.zero)
+		{
+			Stamina -= staminaLostForRunning * Time.deltaTime;
+		}
+
+		Stamina += staminaGenerationPerSecond * Time.deltaTime;
+	}
+
+	void UpdateHealth ()
+	{
+		healthCounter.GetComponent<RectTransform> ().sizeDelta = new Vector2 
+			(healthCounterBackground.GetComponentInParent <RectTransform> ().sizeDelta.x / maxHealth * Health, 
+			healthCounterBackground.GetComponent<RectTransform> ().sizeDelta.y);
+	}
+
+	void UpdateStamina ()
+	{
+		staminaCounter.GetComponent<RectTransform> ().sizeDelta = new Vector2
+			(staminaCounterBackground.GetComponentInParent<RectTransform> ().sizeDelta.x / maxStamina * Stamina,
+			staminaCounterBackground.GetComponent<RectTransform> ().sizeDelta.y);
 	}
 }
